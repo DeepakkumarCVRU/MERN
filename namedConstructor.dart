1b32312e1354216b19188be1d68b5f585a253c88
Deepak kumar <kumardeepak46263@gmail.com>
2025-04-19 16:16:07 +0530

    second commit

--- namedConstructor.dart
+++ namedConstructor.dart
@@ -2,5 +2,8 @@ class Computer {
   int? n1;
   int? n2;
   int? n3;
-  Computer(this.n1, this.n2);
+  int? n4;
+  Computer(this.n1, this.n2, this.n4);
+
+  //kjdfkjndkjnvkfjvkjsfnvkjnknskvnksnksnksnkjfn
 }
